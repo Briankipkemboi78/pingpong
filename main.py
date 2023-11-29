0b1d61ceed621171973550c99ef5ccba73870714
Brian <bkipkemboi@watuafrica.co.ke>
2023-11-29 15:49:58 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,53 @@
+from turtle import Screen
+from paddle import Paddle
+from ball import Ball
+from scoreboard import Scoreboard
+import time
+
+
+screen = Screen()
+screen.setup(width=800, height=600)
+screen.bgcolor("black")
+screen.title("Ping Pong")
+screen.tracer(0)
+
+r_paddle = Paddle((370, 0))
+l_paddle = Paddle((-370, 0))
+ball = Ball((370, 370))
+scoreboard = Scoreboard()
+
+
+screen.listen()
+screen.onkeypress(r_paddle.go_up, "Up")
+screen.onkeypress(r_paddle.go_down, "Down")
+screen.onkeypress(l_paddle.go_up, "w")
+screen.onkeypress(l_paddle.go_down, "s")
+
+
+game_is_on = True
+while game_is_on:
+    time.sleep(ball.move_speed)
+    screen.update()
+    ball.move()
+
+    # Detect with collision
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    # Detect collision with r_paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 340 or ball.distance(l_paddle) < 50 and ball.xcor() < -340:
+        ball.bounce_x()
+
+    # Detect ball missing the r_paddle
+    if ball.xcor() > 370:
+        ball.reset_position()
+        scoreboard.l_point()
+
+    # Detect ball missing the l_paddle
+    if ball.xcor() < -370:
+        ball.reset_position()
+        scoreboard.r_point()
+
+
+
+screen.exitonclick()
